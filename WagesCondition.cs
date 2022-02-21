3be46ba13201c61ae811a4062dc80b1384cf842f
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-21 22:48:53 +0530

    Push file to calculate Monthly condition hour and day

---
+++ WagesCondition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EmployWageComputationProblem
+{
+    public class WagesCondition
+    {
+        public static void calWageTillCondition()
+        {
+            int hrs = 101;
+            int dayHrs = 20 * 8;
+            int calculateWages1;
+            int calculateWages2;
+            for (int count = 1; count < hrs; count++)
+            {
+                
+                calculateWages1 = count * EmployWage.empWagePerHour;
+                Console.WriteLine("Total Working Hour wage for a Month is : {0}", +calculateWages1);
+                if (count < dayHrs)
+                {
+                    calculateWages2 = count * EmployWage.empWagePerHour;
+                    Console.WriteLine("Total Working Day wage for a Month is : {0}", +calculateWages2);
+                    
+                }
+                
+            }
+        }
+    }
+}
