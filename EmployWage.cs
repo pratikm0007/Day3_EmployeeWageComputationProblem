52e5600edd1a505433c743f72ead4a772b319c95
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-21 22:29:20 +0530

    Push daily employ wage file

---
+++ EmployWage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployWageComputationProblem
+{
+    public class EmployWage 
+    {
+        public static int dailyEmployWage;
+        public const int fullDayHours = 8;
+        public const int  empWagePerHour = 20;
+        public static void CalculateEmpWage()
+        {
+           
+            dailyEmployWage = empWagePerHour * fullDayHours;
+            Console.WriteLine("Employ Daily Wage is :{0} ",+ dailyEmployWage);
+            PartTimeEmployWage.findPartTimeWage();
+
+
+
+        }
+
+
+    }
+}
