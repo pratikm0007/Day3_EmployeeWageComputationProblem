7abfb584c9080e450bac05c0f349c40a7f8bf61f
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-21 22:43:53 +0530

    Push a Monthly wage file

---
+++ MonthlyWage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployWageComputationProblem
+{
+    public class MonthlyWage
+    {
+        public const int WorkingDayPerMonth = 20;
+        public static void findMonthlyWage()
+        {
+            int WagesOfMonth = WorkingDayPerMonth * EmployWage.dailyEmployWage;
+            Console.WriteLine("Wages for a Month is {0}",+ WagesOfMonth);
+            WagesCondition.calWageTillCondition();
+
+        }
+    }
+}
