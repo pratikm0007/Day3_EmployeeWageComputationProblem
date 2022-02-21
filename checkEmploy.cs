e3e32209ee3715f9003cf08c573729c9c53b32d6
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-21 22:23:40 +0530

    Push attendance check file

---
+++ checkEmploy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace EmployWageComputationProblem
+{
+    public class checkEmploy
+    {
+        public static void EmployAbsentOrPresent()
+        {
+            Random random = new Random();
+            int presenty = random.Next(2);
+
+            if (presenty == 1)
+            {
+                Console.WriteLine("Employ is Present");
+                EmployWage.CalculateEmpWage();
+            }
+            else if (presenty == 0)
+            {
+                Console.WriteLine("Employ is Absent");
+                Console.WriteLine("Employ per Day Wage is : 0");
+            }
+
+        }
+    }
+}
